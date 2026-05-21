b81c284ca195c0faddf0491228ef81e8fab84e76
msteslov <msteslov@edu.hse.ru>
2026-05-21 11:57:45 +0300

    Add finite field function stubs

---
+++ src/finite_field.py
@@ -0,0 +1,26 @@
+def add(a: int, b: int, p: int) -> int:
+    pass
+
+
+def sub(a: int, b: int, p: int) -> int:
+    pass
+
+
+def mul(a: int, b: int, p: int) -> int:
+    pass
+
+
+def inv(a: int, p: int) -> int:
+    pass
+
+
+def div(a: int, b: int, p: int) -> int:
+    pass
+
+
+def is_prime(p: int) -> bool:
+    pass
+
+
+def normalize(a: int, p: int) -> int:
+    pass
