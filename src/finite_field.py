82dc724563758ce546cf981ae916736c53b2053f
msteslov <msteslov@edu.hse.ru>
2026-05-21 12:05:50 +0300

    Implement finite field basics

--- src/finite_field.py
+++ src/finite_field.py
@@ -1,21 +1,21 @@
 def add(a: int, b: int, p: int) -> int:
-    pass
+    return (a + b) % p
 
 
 def sub(a: int, b: int, p: int) -> int:
-    pass
+    return (a - b) % p
 
 
 def mul(a: int, b: int, p: int) -> int:
-    pass
+    return (a * b) % p
 
 
 def inv(a: int, p: int) -> int:
-    pass
+    return pow(a, -1, p)
 
 
 def div(a: int, b: int, p: int) -> int:
-    pass
+    return mul(a, inv(b, p)) % p
 
 
 def is_prime(p: int) -> bool:
@@ -23,4 +23,4 @@ def is_prime(p: int) -> bool:
 
 
 def normalize(a: int, p: int) -> int:
-    pass
+    return a % p
